bd40c3a80edfbef186f688f26d78580a8bd9aa68
Keegan Ritchie <keegan.ritchie0@gmail.com>
2022-05-07 15:54:52 -0600

    Started sensor integration

--- arduino_baremetal/arduino_baremetal.ino
+++ arduino_baremetal/arduino_baremetal.ino
@@ -1,13 +1,27 @@
+/*
+ * File: arduino_baremetal.ino
+ * 
+ * Version: 0.0.0
+ * Author: Keegan Ritchie
+ * Date: May 7 2022
+ */
+
+/*
+ * Architecture: AVR
+ * Microcontroller: ATmega328
+ * Clock Speed: 16 MHz
+ */
+
 // Application Libraries
+#include "/Users/kritchie0/Developer/Foundation/gpio.h"
 
 // Arduino Libraries
 #include <avr/io.h>
 
-
 #define LED1 (1 << 5)
 
-static void bit_set(volatile unsigned char *dst, uint8_t bit_mask);
-static void bit_clear(volatile unsigned char *dst, uint8_t bit_mask);
+//static void bit_set(volatile unsigned char *dst, uint8_t bit_mask);
+//static void bit_clear(volatile unsigned char *dst, uint8_t bit_mask);
 
 void setup() 
 {
@@ -19,7 +33,7 @@ void loop()
 {
 //NOTHING HERE
 }
-
+#if 0
 // Local Functions
 static void bit_set(volatile unsigned char *dst, uint8_t bit_mask)
 {
@@ -30,3 +44,4 @@ static void bit_clear(volatile unsigned char *dst, uint8_t bit_mask)
 {
   *dst &= (~bit_mask);
 }
+#endif
